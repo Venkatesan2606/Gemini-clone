1d7c48e600efef84537ffe7f9ef94f287f3f776f
venkat <venkatanbu723@gmail.com>
2025-03-05 11:47:31 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Sidebar from "./components/Sidebar/sidebar"
+import Main from "./components/Main/Main"
+
+function App() {
+ return (
+ <>
+  <Sidebar/>
+  <Main/>
+ </>
+  )
+}
+
+export default App
